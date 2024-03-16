9d0ade7fdbeb572cb17a2efa64797dee9ca196c1
MuAnnan <102162062+MuAnnan@users.noreply.github.com>
2024-03-16 15:47:46 +0800

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -7,6 +7,7 @@ function startRecord() {
     const elem = document.getElementById("userContent")
     elem.innerHTML = text
     ALTabletBinding.raiseEvent("开始录音")
+    console.log("record start")
 }
 
 function overRecord() {
@@ -17,4 +18,5 @@ function overRecord() {
     const elem = document.getElementById("userContent")
     elem.innerHTML = text
     ALTabletBinding.raiseEvent("结束录音")
+    console.log("record over")
 }
